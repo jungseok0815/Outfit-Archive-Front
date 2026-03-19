43eb84319e96104b359854dcc3f9c657ab83d9c3
DESKTOP-CB5R3AI\USER <wjdtjr9401@naver.com>
2026-03-19 16:49:22 +0900

    add ec2 server

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: process.env.REACT_APP_API_URL,
     withCredentials: true,  // 쿠키 처리를 위해 필수
 })
 
